73a094272f310f63771123b276f3805b790bd417
Kai Chen <279414932@qq.com>
2020-08-22 18:24:21 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # My-Leetcode
-My own leetcode history
+## Note
+
+### 回溯法
+
+- 回溯法：
+
+  - 通过不停的选择，撤销选择，来穷尽所有可能性，最后将满足条件的结果全部返回
+  - 是DFS的一种，实际上就是先序遍历
+  - 是一种纯暴力搜索算法，时间复杂度一般是O(N!)
+  - 模板：
+
+  ```python
+  result = []
+  func backtrack(选择列表,路径):
+      if 满足结束条件:
+          result.add(路径)
+          return
+      for 选择 in 选择列表:
+          做选择
+          backtrack(选择列表,路径)
+          撤销选择
+  ```
+
+
+
--- python/78. Subsets.py
+++ python/78. Subsets.py
@@ -6,7 +6,7 @@ class Solution:
     """
 
     """
-    Solution 1:
+    Solution 1: blue force
     for each new element, create a new uni-set of it and add it to every current subset
     """
     def subsets(self, nums: List[int]) -> List[List[int]]:
@@ -17,6 +17,17 @@ class Solution:
         return result
 
     """
-    Solution 2: 
-    Backtrace
+    Solution 2: Backtrace
     """
+    def __init__(self):
+        self.result = []
+        self.temp = []
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.backtrace(nums, 0)
+        return self.result
+    def backtrace(self, nums, start):
+        self.result.append(self.temp[:])
+        for i in range(start, len(nums)):
+            self.temp.append(nums[i])
+            self.backtrace(nums, i + 1) # 前面的都已经搜索过了
+            self.temp = self.temp[:-1]
